14b90fba3de4d93596bef0a2d4698308fe6c6730
Sam Niyitanga <niyitangasam@gmail.com>
2016-03-16 16:49:05 +0200

    change in client

--- resources/views/clients/footer.blade.php
+++
@@ -1,7 +0,0 @@
-   <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Designed & Develloped by Sam and Fred copy; tskills 2016</p>
-                </div>
-            </div>
-        </footer>
