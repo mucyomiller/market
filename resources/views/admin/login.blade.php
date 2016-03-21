be7696b69955a382b74a318020dd504a05e07cce
Mucyo Miller <mucyomiller@gmail.com>
2016-03-21 10:18:17 +0200

    some improvement on design

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -65,7 +65,7 @@
         </form>
 
         <div class="social-auth-links text-center">
-          <p>- OR -</p>
+          <p>You may also </p>
           <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
           <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
         </div><!-- /.social-auth-links -->
