d85e7c6a27f540e68dcb6189449c3f3c6fcd80ce
Mucyo Miller <mucyomiller@gmail.com>
2016-04-07 20:39:08 +0200

    version change

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,6 +1,6 @@
     <footer class="main-footer">
     <div class="pull-right hidden-xs">
-    <b>Version</b> 1.0.0
+    <b>Version</b> 1.2.0
     </div>
     <center><strong>&copy;&nbsp;&nbsp;Copyright 2016 <a href="http://tskillsc.com">tskillsc ltd.</a></strong> All rights reserved.</center>
     </footer>
